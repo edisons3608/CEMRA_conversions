2c143c072a1b0110761827aa84fd6452b8270fb8
Edison Sun <edisonsun2028@u.northwestern.edu>
2025-06-22 11:44:34 -0500

    add optional mask functionality

--- overlaymr.m
+++ overlaymr.m
@@ -1,7 +1,15 @@
 function out = overlaymr(image,mask)
     addpath(fullfile(fileparts(mfilename('fullpath')), 'imtool3D'));
-    
-    tool = imtool3D(double(image.dataAy));
-    setMask(tool,double(mask.dataAy));
+    if nargin < 1
+        error("Please provide at least an input volume.")
+      
+    end
+    if nargin < 2
+        tool = imtool3D(double(image.dataAy));
+    else
+        tool = imtool3D(double(image.dataAy));
+        setMask(tool,double(mask.dataAy));
+      
+    end
     
 end
