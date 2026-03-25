fbf4a6385958ded1b32e3e5b1c0afec4aa90104b
Edison Sun <57467707+edisons3608@users.noreply.github.com>
2026-03-25 14:47:12 -0500

    push ratio edit

--- mip.m
+++ mip.m
@@ -1,19 +1,23 @@
-
 function mip(vol)
     
     if strcmp(class(vol),'struct')
-        A = max(mrStruct.dataAy,[],2);
+        A = max(vol.dataAy,[],2);
         A = squeeze(A);
-    
-        image(A);
-    elseif strcmp(class(vol),'string')
+        vox = vol.vox;
+
+        y = (0:size(A,1)-1) * vox(1);
+        x = (0:size(A,2)-1) * vox(3);
+        imagesc(x, y, A);
+        axis image;
+    elseif strcmp(class(vol),'string') || strcmp(class(vol),'char')
+        info = niftiinfo(vol);
         A = double(niftiread(vol));
         A = max(A,[],2);
         A = squeeze(A);
-        image(A);
+        vox = info.PixelDimensions;
+
+        y = (0:size(A,1)-1) * vox(1);
+        x = (0:size(A,2)-1) * vox(3);
+        imagesc(x, y, A);
+        axis image;
     end
-
-
-
-
-end
